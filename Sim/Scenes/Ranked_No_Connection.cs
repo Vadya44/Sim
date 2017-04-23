c5c9333cfae3289c04fd984eea34139529054b90
Vadya44 <44vadimko@gmail.com>
2017-04-23 21:46:53 +0300

    Fixed bad idea

--- Sim/Scenes/Ranked_No_Connection.cs
+++ Sim/Scenes/Ranked_No_Connection.cs
@@ -4,15 +4,15 @@ namespace Sim
 {
 	public class Ranked_No_Connection
 	{
-		static void OnDraw(Canvas canvas)
+		public static void OnDraw(Canvas canvas)
 		{
 
 		}
-		static void Show()
+		public static void Show()
 		{
 			GameView.DrawEvent += OnDraw;
 		}
-		static void Hide()
+		public static void Hide()
 		{
 			GameView.DrawEvent -= OnDraw;
 		}
