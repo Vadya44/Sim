a6396e413e8f29297d716b0aae2f34ae474284c5
Vadya44 <44vadimko@gmail.com>
2017-04-25 21:10:43 +0300

    TouchHandler's methods is Finished too

--- Sim/TouchHandler.cs
+++ Sim/TouchHandler.cs
@@ -21,13 +21,73 @@ namespace Sim
                 case "MainMenu":
                     MainMenu.JustTouch(x, y);
                     break;
+                case "Account":
+                    Account.JustTouch(x, y);
+                    break;
+                case "Authentication":
+                    Account.JustTouch(x, y);
+                    break;
+                case "Game_Ranked":
+                   Game_Ranked.JustTouch(x, y);
+                    break;
+                case "Game_Solo":
+                    Game_Solo.JustTouch(x, y);
+                    break;
+                case "LeaderBoard":
+                    LeaderBoard.JustTouch(x, y);
+                    break;
+                case "Ranked_No_Connection":
+                    Ranked_No_Connection.JustTouch(x, y);
+                    break;
+                case "Result_Ranked":
+                    Result_Ranked.JustTouch(x, y);
+                    break;
+                case "Result_Solo":
+                    Result_Solo.JustTouch(x, y);
+                    break;
+                case "Rules":
+                    Rules.JustTouch(x, y);
+                    break;
                 default:
                     break;
             }
         }
         public static void MovedTouch(float x1, float y1, float x2, float y2)
         {
-            MainMenu.MovedTouch(x1, y1, x2, y2);
+            switch (GameView.activeScene)
+            {
+                case "MainMenu":
+                    MainMenu.MovedTouch(x1, y1, x2, y2);
+                    break;
+                case "Account":
+                    Account.MovedTouch(x1, y1, x2, y2);
+                    break;
+                case "Authentication":
+                    Account.MovedTouch(x1, y1, x2, y2);
+                    break;
+                case "Game_Ranked":
+                    Game_Ranked.MovedTouch(x1, y1, x2, y2);
+                    break;
+                case "Game_Solo":
+                    Game_Solo.MovedTouch(x1, y1, x2, y2);
+                    break;
+                case "LeaderBoard":
+                    LeaderBoard.MovedTouch(x1, y1, x2, y2);
+                    break;
+                case "Ranked_No_Connection":
+                    Ranked_No_Connection.MovedTouch(x1, y1, x2, y2);
+                    break;
+                case "Result_Ranked":
+                    Result_Ranked.MovedTouch(x1, y1, x2, y2);
+                    break;
+                case "Result_Solo":
+                    Result_Solo.MovedTouch(x1, y1, x2, y2);
+                    break;
+                case "Rules":
+                    Rules.MovedTouch(x1, y1, x2, y2);
+                    break;
+                default:
+                    break;
+            }
         }
     }
-}
